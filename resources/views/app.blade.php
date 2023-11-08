1376dc7c6ed514462b536a0b54478fda0ab4638d
daniel22alejandro <danniel10yara@gmail.com>
2023-11-08 14:15:26 -0500

    aaaaaaa

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Laravel</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+        </head>
+        <body class="bg-white-gray-100">
+            
+       <header class="p-5 border-b bg-white shadow">
+        <h1  class="border-b text-3xl font-bold  ">HOLA MUNDO</h1>
+    </header>
+
+    </body>
+</html>
